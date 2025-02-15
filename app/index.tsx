d9e7946415190c78595f77e79131cf83931481ab
Muhammad Talha Saeed <iamtalha971@gmail.com>
2025-02-15 18:01:15 +0400

    feat: Data Storage Done

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,6 @@
 import { data } from "@/utils/db/todos";
-import { useContext, useState } from "react";
-import {
-  FlatList,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { TodoType } from "@/types/todo";
@@ -15,15 +8,53 @@ import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
 import { ThemeContext, ThemeContextProps } from "@/context/ThemeContext";
 import { ThemeType } from "@/constants/Colors";
 import { Octicons } from "@expo/vector-icons";
+import Animated, { LinearTransition } from "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
-  // Theme Context
+
+  //? Context
   const { theme, colorScheme, setColorScheme } = useContext(
     ThemeContext
   ) as ThemeContextProps;
-  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  
+  //? States
+  const [todos, setTodos] = useState<TodoType[]>([]);
   const [text, setText] = useState("");
   const [loaded, error] = useFonts({ Inter_500Medium });
+
+  //! Async Storage Get All Stored Data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonData = await AsyncStorage.getItem("TodoApp");
+        const storedTodos: TodoType[] = !jsonData ? null : JSON.parse(jsonData);
+
+        if (storedTodos && storedTodos.length) {
+          setTodos(storedTodos.sort((a, b) => b.id - a.id));
+        } else {
+          setTodos(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  //! Async Storage Get Update Stored Data
+  useEffect(() => {
+    const updateData = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos);
+        await AsyncStorage.setItem("TodoApp", jsonValue);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    updateData();
+  }, [todos]);
 
   if (!loaded && !error) {
     return null;
@@ -57,7 +88,7 @@ export default function Home() {
   };
 
   //! Styles
-  const styles = createStyles(theme, colorScheme)
+  const styles = createStyles(theme, colorScheme);
 
   //! Render Item
   const renderItem = ({ item }: { item: TodoType }) => (
@@ -81,17 +112,19 @@ export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
+        {/* Search Input  */}
         <TextInput
           placeholder="Add a new todo"
           style={styles.input}
           placeholderTextColor="gray"
           value={text}
           onChangeText={setText}
-        />
+          />
+          {/* Add Button */}
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
-        {/* //? Theme Toggle Button */}
+        {/*  Theme Toggle Button */}
         <Pressable
           onPress={() =>
             setColorScheme(
@@ -107,78 +140,91 @@ export default function Home() {
           )}
         </Pressable>
       </View>
-      <FlatList
+
+      {/* Todo List  */}
+      <Animated.FlatList
         data={todos}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={{ flexGrow: 1 }}
-        ListEmptyComponent={<View style={[styles.todoItemText,{marginHorizontal:"auto"}]}>Not found!</View>}
+        ListEmptyComponent={
+          <View style={[styles.todoItemText, { marginHorizontal: "auto" }]}>
+            <Text>Not found! Please Add New</Text>
+          </View>
+        }
+        itemLayoutAnimation={LinearTransition}
+        keyboardDismissMode="on-drag"
+      />
+      //! Status Bar Top of Device i.e time, % signal
+      <StatusBar
+        style={
+          colorScheme === ThemeType.dark ? ThemeType.light : ThemeType.dark
+        }
       />
     </SafeAreaView>
   );
 }
 
-const createStyles = (theme:any, colorScheme:ThemeType) =>{
-return StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: theme.background,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    padding: 10,
-    width: "100%",
-    maxWidth: 1024,
-    marginHorizontal: "auto",
-    pointerEvents: "auto",
-  },
-  input: {
-    flex: 1,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    fontSize: 18,
-    fontFamily: "Inter_500Medium",
-    minWidth: 0,
-    color: theme.text,
-  },
-  addButton: {
-    backgroundColor: theme.button,
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText: {
-    fontSize: 18,
-    color: colorScheme === 'dark' ? 'black': 'white',
-  },
-  todoItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: 4,
-    padding: 10,
-    borderBottomColor: "gray",
-    borderBottomWidth: 1,
-    width: "100%",
-    maxWidth: 1024,
-    marginHorizontal: "auto",
-    pointerEvents: "auto",
-  },
-  todoItemText: {
-    fontFamily: "Inter_500Medium",
-    flex: 1,
-    fontSize: 18,
-    color: theme.text,
-  },
-  completedText: {
-    textDecorationLine: "line-through",
-    color: "gray",
-  },
-});
-}
-
+const createStyles = (theme: any, colorScheme: ThemeType) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      padding: 10,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    input: {
+      flex: 1,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
+      minWidth: 0,
+      color: theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: colorScheme === "dark" ? "black" : "white",
+    },
+    todoItem: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 4,
+      padding: 10,
+      borderBottomColor: "gray",
+      borderBottomWidth: 1,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    todoItemText: {
+      fontFamily: "Inter_500Medium",
+      flex: 1,
+      fontSize: 18,
+      color: theme.text,
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      color: "gray",
+    },
+  });
+};
--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -2,18 +2,17 @@ import { createContext, useState } from "react";
 import { Appearance } from "react-native";
 import { Colors, ThemeType } from "@/constants/Colors";
 
-
 export interface ThemeContextProps {
-  theme:any,
-  colorScheme:ThemeType
-  setColorScheme: (color:ThemeType)=> void
+  theme: any;
+  colorScheme: ThemeType;
+  setColorScheme: (color: ThemeType) => void;
 }
 export const ThemeContext = createContext<ThemeContextProps | {}>({});
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
-console.log({colorScheme})
-  const theme = colorScheme === ThemeType.dark  ? Colors.dark : Colors.light;
+
+  const theme = colorScheme === ThemeType.dark ? Colors.dark : Colors.light;
   return (
     <ThemeContext.Provider value={{ colorScheme, setColorScheme, theme }}>
       {children}
